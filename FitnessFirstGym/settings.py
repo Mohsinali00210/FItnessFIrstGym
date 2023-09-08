79e64d0f3e218b3be244b2150e10348d12096555
mohsin ali <mohsinali00210@gmail.com>
2023-09-08 01:45:48 -0700

    daily update

--- FitnessFirstGym/settings.py
+++ FitnessFirstGym/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website'
+    'fontawesomefree',
+    'website',
+    'dashboard'
 ]
 
 MIDDLEWARE = [
@@ -120,8 +122,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_ROOT = BASE_DIR / 'dashboard'
+STATIC_URL =  'static/'
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'mystatic'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
