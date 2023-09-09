dbc8c56acd907d8170acace162554cb0505800f1
mohsin ali <mohsinali00210@gmail.com>
2023-09-08 23:56:49 -0700

    daily update

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    Name = models.CharField(max_length=100)
+    PhoneNo = models.CharField(max_length=11)
+    Email = models.EmailField(max_length=100)
+    Address = models.CharField(max_length=150)
+    DateOfBirth = models.DateField()
+    Gender = models.CharField(max_length=100)
+    LockerAssignment = models.CharField(max_length=100)
+
+
+class RegisteredUser(models.Model):
+    PersonId = models.ForeignKey(Person,on_delete=models.CASCADE)
+    FitnessGoals=models.CharField(max_length=100)
+    Image=models.ImageField(upload_to='images/')
+
+
+
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib import messages 
+from . import models
 
 # Create your views here.
 def dashboard(request):
-    return render(request, 'base.html')
+    if request.method == 'POST':
+        useremail = request.POST['useremail']
+        password = request.POST['password']
+		# Authenticate
+        user = authenticate(request, username=useremail, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('dashboard')
+        else:
+            messages.success(request, "There Was An Error Logging In, Please Try Again...")
+            return redirect('dashboard')
+
+    else:
+        return render(request, 'home.html')
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You Have Been Logged Out...")
+    return redirect('dashboard')
+
+
+def register_view(request):
+    if request.method=='POST':
+        FullName = request.POST.get('FullName')
+        Email = request.POST.get('Email')
+        Gender = request.POST.get('Gender')
+        Phone = request.POST.get('Phone')
+        Address = request.POST.get('Address')
+        DateOfBirth = request.POST.get('DateOfBirth')
+        FitnessGoals = request.POST.get('FitnessGoals')
+        password = request.POST.get('password')
+        profileP = request.FILES['ProfilePic']
+        
+        person = models.Person(Name=FullName,PhoneNo=Phone,Email=Email,Address=Address,DateOfBirth=DateOfBirth,Gender=Gender)
+        person.save()
+
+        registeredUser = models.RegisteredUser(PersonId=person,FitnessGoals=FitnessGoals,Image=profileP)
+        registeredUser.save()
+    return render(request,'register.html')
